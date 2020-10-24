dd43459019837c57e2222f8cebebf8025e73f49c
Rush Morgan <51529432+Ounza@users.noreply.github.com>
2020-10-24 16:11:47 +0300

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,44 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+        if(callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+            
+        }
+    }
+    start= () => {
+        if (this.onStart){
+            this.onStart(this.timeRemaining);
+        }
+        this.tick(); //call tick for the first time
+        this.interval = setInterval(this.tick, 20); //calls tick after one second
+    }
+    pause = () => {
+        clearInterval(this.interval);
+    }
+    tick = () => {
+        if(this.timeRemaining <= 0){
+            this.pause();
+            if (this.onComplete){
+                this.onComplete();
+            }
+        }else{
+        this.timeRemaining = this.timeRemaining - 0.02;
+        if (this.onTick){
+            this.onTick(this.timeRemaining);
+        }
+        }
+    }
+    get timeRemaining(){
+        return parseFloat(this.durationInput.value);
+    }
+    set timeRemaining(time){
+        this.durationInput.value = time.toFixed(2);
+    }
+}
